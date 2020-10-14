7abcbdc1d7ea9ce1773d7f226c0414794b597999
kaidenlewis <72454398+kaidenlewis@users.noreply.github.com>
2020-10-14 09:35:47 -0500

    Update Debug6.cpp

--- Debug6.cpp
+++ Debug6.cpp
@@ -17,17 +17,18 @@ meant to check if the code is actually in order.
 int arr[] = {20,12,65,45,98,65,85,12,54,78,55,13,50,19,84,20,43,10};
 int arrSize =  sizeof(arr)/sizeof(arr[0]);
 int main() {
-
+  int temp ;
+  unsigned i;
   //Bit of code for sorting
   for (unsigned j=arrSize;j>0;j--){
     for (unsigned i=1;i<j;i++){
       if (arr[i-1]>arr[i]){
+        temp = arr [i-1] ;
         arr[i-1] = arr[i];
-        arr[i] = arr[i-1];
+        arr[i] = temp ;
         }
     }
   }
-
   // Bit of code to check if sort worked
   // HINT : This bit of code is just fine
   bool checkFlag = true;
@@ -36,7 +37,7 @@ int main() {
   }
   if (checkFlag) {cout << "Sorted properly";}
   else {cout << "Not sorted properly";}
-  cout << endl << arr[0];
+  cout << endl << arr[i];
   for (unsigned i=1;i<arrSize;i++){
     cout << ", " << arr[i];
   }
